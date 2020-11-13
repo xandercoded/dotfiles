d2376ddcdc89e8d3fe36c84d328b84e9e98468b6
xandercoded <xandercoded@gmail.com>
2020-11-13 12:40:12 -0500

    Adds ip-in-cidr helper

--- .helpers.sh
+++ .helpers.sh
@@ -3,8 +3,13 @@
 source ~/extras/helpers.sh
 source ~/forgit.plugin.sh
 
-http_check_secure_upgrade() {
-  r=$(curl --head -sLI "$1" | grep 'HTTP/\d\.\d|Location')
+fzf_process_kill() {
+  kill -9 $(ps aux | fzf | awk '{print $2}')
+}
+
+net_check_http_secure_upgrade() {
+  local r=$(curl --head -sLI "$1" | grep 'HTTP/\d\.\d|Location')
+
   if echo "$r" | grep -Eq "301" && echo "$r" | grep -Eq 'Location:\s+https:\/\/'; then
     echo redirected
     return 0
@@ -14,6 +19,15 @@ http_check_secure_upgrade() {
   fi
 }
 
-fzf_process_kill() {
-  kill -9 $(ps aux | fzf | awk '{print $2}')
+net_ip_in_cidr() {
+  local ip=$1
+  local cidr=$2
+
+  if nmap -sL -n "$cidr" | awk '/Nmap scan report/{print $NF}' | grep -q "$ip\b"; then
+    echo found
+    return 0
+  else
+    echo "not found"
+    return 1
+  fi
 }
